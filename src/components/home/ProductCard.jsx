975ef1307d2e857bbdc1cf8f9eea19a40f995d23
Alan Yeverino <lanyev@gmail.com>
2023-01-07 20:57:33 -0600

    correccion css

--- src/components/home/ProductCard.jsx
+++ src/components/home/ProductCard.jsx
@@ -1,44 +1,52 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { addProductCart } from '../../store/slices/cart.slice'
-import "./style/ProductCard.css"
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addProductCart } from "../../store/slices/cart.slice";
+import "./style/productCard.css";
 
-const ProductCard = ({product}) => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+const ProductCard = ({ product }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleClickProduct = () => {
-    navigate(`/products/${product.id}`)
-  }
+    navigate(`/products/${product.id}`);
+  };
 
-  const handleClickAddCart = e => {
-    e.stopPropagation()
+  const handleClickAddCart = (e) => {
+    e.stopPropagation();
     const data = {
       id: product.id,
-      quantity: 1
-    }
-    dispatch(addProductCart(data))
-    alert("you have added a product to the cart")
-  }
+      quantity: 1,
+    };
+    dispatch(addProductCart(data));
+    alert("you have added a product to the cart");
+  };
 
   return (
-    <article className='productCard' onClick={handleClickProduct}>
-      <header className='productCard__header'>
-        <img className='productCard__header-img1' src={product.productImgs[0]} alt="product" />
-        <img className='productCard__header-img2' src={product.productImgs[1]} alt="products" />
+    <article className="productCard" onClick={handleClickProduct}>
+      <header className="productCard__header">
+        <img
+          className="productCard__header-img1"
+          src={product.productImgs[0]}
+          alt="product"
+        />
+        <img
+          className="productCard__header-img2"
+          src={product.productImgs[1]}
+          alt="products"
+        />
       </header>
       <hr />
-      <div className='productCard__boby'>
-        <h3 className='productCard__title'>{product.title}</h3>
-        <h4 className='productCard__title-price'>Price</h4>
-        <span className='productCard__price'>$ {product.price}</span>
-        <button className='productCard__btn' onClick={handleClickAddCart}>
-          <i className='bx bx-cart'></i>
+      <div className="productCard__boby">
+        <h3 className="productCard__title">{product.title}</h3>
+        <h4 className="productCard__title-price">Price</h4>
+        <span className="productCard__price">$ {product.price}</span>
+        <button className="productCard__btn" onClick={handleClickAddCart}>
+          <i className="bx bx-cart"></i>
         </button>
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
